da9f8a47765ce99e1e5b4b87896ee5d0767f06f7
Qmo <Qmo.Chiu@senao.com.tw>
2017-09-12 13:44:48 +0800

    reset component

---
+++ app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { AppPage } from './app.po';
+import { protractor } from 'protractor/built/ptor';
+
+describe('demo App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('homework02 works!');
+  });
+});
+
---
+++ app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-homework02 h1')).getText();
+  }
+}
