58165bbfd8b9d8c0b8b09d51fcf2007928411ecb
Unknown <qhairmonster@gmail.com>
2017-09-13 17:03:05 +0800

    整合測試紅燈

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,32 @@ describe('homework02 App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  // it('should display welcome message', () => {
+  //   page.navigateTo();
+  //   expect(page.getParagraphText()).toEqual('Welcome to app!');
+  // });
+
+  it('should have 3 options', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+    expect(page.getSelectOptionsCount()).toEqual(3);
+  })
+
+  it('if select AWS should display 0', () => {
+    page.navigateTo();
+    page.getSelectOptionText().toEqual('AWS');
+    expect(page.getValue()).toBe('0');
+  })
+
+  it('if select AWS should display 1', () => {
+    page.navigateTo();
+    page.getSelectOptionText().toEqual('Azure');
+    expect(page.getValue()).toBe('0');
+  })
+
+  it('if select AWS should display 2', () => {
+    page.navigateTo();
+    page.getSelectOptionText().toEqual('Aliyun');
+    expect(page.getValue()).toBe('0');
+  })
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,25 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+  value: string;
+
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  // getParagraphText() {
+  //   return element(by.css('app-root h1')).getText();
+  // }
+
+  getSelectOptionsCount(): any {
+    return element(by.css('app-root select')).length;
+  }
+
+  getSelectOptionText(): any {
+    return element(by.css('app-root select')).$('option:checked').getText();
+  }
+  
+  getValue(): any {
+    return this.value;
   }
 }
